ca37cc0a6077621a70d38915848883b946cf197a
Jan Thiel <jan@hive-it.de>
2025-08-11 23:20:30 +0200

    Refactor garbage collection process for persistent transients

--- src/includes/garbage-collection.php
+++ src/includes/garbage-collection.php
@@ -18,12 +18,15 @@ add_action( 'init', __NAMESPACE__ . '\maybe_schedule_event' );
 
 /**
  * Garbage collection function.
+ * Select all persistent transient options and delete those that have expired.
+ * Using the SQL query to select options that match the persistent transient prefix and have an expiration timestamp less than the current time is much faster than the previous approach.
  */
 function collect_garbage() {
 	global $wpdb;
 
 	$option_prefix = Helpers\get_option_prefix();
-	$query         = sprintf( "SELECT option_name from {$wpdb->options} WHERE `option_name` LIKE '%s%%'", $option_prefix );
+    $expiry_query = "{$option_prefix}%_expiration";
+	$query         = $wpdb->prepare( "SELECT option_name from {$wpdb->options} WHERE `option_name` LIKE '%s' AND CAST(option_value AS UNSIGNED) < UNIX_TIMESTAMP()", $expiry_query );
 
 	$option_names  = $wpdb->get_col( $query );
 
@@ -32,16 +35,9 @@ function collect_garbage() {
 	}
 
 	foreach ( $option_names as $option_name ) {
-
-		if ( preg_match( '/_expiration$/', $option_name ) ) {
-			continue;
-		}
-
-		$transient_name = preg_replace( "/^$option_prefix/", '', $option_name );
-
-		if ( Helpers\is_transient_expired( $transient_name ) ) {
-			PersistentTransients\delete( $transient_name );
-		}
+        $transient_name = str_replace($option_name, '', '_expiration');
+        PersistentTransients\delete( $transient_name );
+        PersistentTransients\delete( $option_name );
 	}
 }
 add_action( Helpers\get_garbage_collection_hook_name(), __NAMESPACE__ . '\collect_garbage' );
