1cd74ad260afa65f73089eb58bbf1c4c5884cca9
Jan Thiel <jan@hive-it.de>
2025-08-11 23:25:46 +0200

    Refactor garbage collection process for persistent transients

--- src/includes/garbage-collection.php
+++ src/includes/garbage-collection.php
@@ -20,28 +20,23 @@ add_action( 'init', __NAMESPACE__ . '\maybe_schedule_event' );
  * Garbage collection function.
  */
 function collect_garbage() {
-	global $wpdb;
+    global $wpdb;
 
-	$option_prefix = Helpers\get_option_prefix();
-	$query         = sprintf( "SELECT option_name from {$wpdb->options} WHERE `option_name` LIKE '%s%%'", $option_prefix );
+    $option_prefix = Helpers\get_option_prefix();
+    $expiry_query = "{$option_prefix}%_expiration";
+    $query         = $wpdb->prepare( "SELECT option_name from {$wpdb->options} WHERE `option_name` LIKE '%s' AND CAST(option_value AS UNSIGNED) < UNIX_TIMESTAMP()", $expiry_query );
 
-	$option_names  = $wpdb->get_col( $query );
+    $option_names  = $wpdb->get_col( $query );
 
-	if ( empty( $option_names ) ) {
-		return;
-	}
+    if ( empty( $option_names ) ) {
+        return 0;
+    }
 
-	foreach ( $option_names as $option_name ) {
-
-		if ( preg_match( '/_expiration$/', $option_name ) ) {
-			continue;
-		}
-
-		$transient_name = preg_replace( "/^$option_prefix/", '', $option_name );
-
-		if ( Helpers\is_transient_expired( $transient_name ) ) {
-			PersistentTransients\delete( $transient_name );
-		}
-	}
+    foreach ( $option_names as $option_name ) {
+        $transient_name = str_replace('_expiration', '', $option_name);
+        delete_option( $transient_name );
+        delete_option( $option_name );
+    }
+    return count($option_name);
 }
 add_action( Helpers\get_garbage_collection_hook_name(), __NAMESPACE__ . '\collect_garbage' );
